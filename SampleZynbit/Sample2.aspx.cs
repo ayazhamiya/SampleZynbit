79f12446c3ea071ec8b4c9e9d2b5a2f298714648
ayaz shahid <ayazhamiya@gmail.com>
2018-07-17 17:58:18 +0400

    jhg

---
+++ SampleZynbit/Sample2.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SampleZynbit
+{
+    public partial class Sample2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
